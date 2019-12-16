0569de61d60d377b36c44eb904ea7dc867a99c37
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-12-16 20:13:53 +0200

    16122019_1

--- pyqtilk.py
+++ pyqtilk.py
@@ -1,15 +1,39 @@
-from PyQt5.QtWidgets import QMainWindow,QApplication,QPushButton
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+
+
+
 class App(QMainWindow):
+
     def __init__(self):
         super().__init__()
-        self.button = QPushButton(text="deneme")
+        self.win =  uic.loadUi(r"GUI\AnaMenu.ui")
+        self.win.btYeni.clicked.connect(self.tiklandi)
+        self.win.cmbMusteri.currentIndexChanged.connect(self.degisti)
+        self.comboDoldur()
+        self.initUI()
         
-        self.show()
+    def initUI(self):
+        self.win.show()
 
+    def comboDoldur(self): 
+        for i in range(0,5):
+            self.win.cmbMusteri.addItem("Musteri"+str(i),str(i))
+    
+    def tiklandi(self):
+        print("buton çalıştı")
 
+    def degisti(self):
+        secilen = self.win.cmbMusteri.currentIndex()
+        print(secilen)
 
-if __name__ == "__main__":
+          
+    
+    
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    exe = App()
-    sys.exit(app.exec_())
+    ex = App()
+    sys.exit(app.exec_()) 
+
